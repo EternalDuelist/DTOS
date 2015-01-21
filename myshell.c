0546c01a1bdfa351aae1630c6e3792f426cf04df
EternalDuelist <davcarranza@gmail.com>
2015-01-21 15:21:27 -0800

    Modified some stuff

--- myshell.c
+++ myshell.c
@@ -1,40 +1,59 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <errno.h>
 #include <string.h>
 
-extern char **getline();
-pid_t pid;
+extern char **getline(void);
 
-void commands (char *c, char **a) {
-   int status;
-   if (pid == 0) {
-      execvp(c, a);
+void exeCommand (char *command, char **args){
+   if (strcmp(command, "cd") == 0) {
+      chdir(args[0]);
+   } else {
+      execvp(command, args);
    }
-   else {
+}
+
+void newProc (char *command, char **args) {
+   pid_t pid;
+   int status;
+   int i = 0;
+   
+   pid = fork();
+
+   if (pid == 0) {
+      exeCommand(command, args);
+   } else {
       waitpid(pid, &status,0);
+      /*
+      printf("Command: %s Arguments:",c); 
+      while(a[i] != NULL){
+         printf("%s ",a[i]);
+         i++;
+      } 
+      printf("\n");
+      */
    }
 }
 
 int main (void) {
    int i;
    char **args;
-   
+
    while(1) {
-      printf("swagshell> ");
+      printf("SwagShell> ");
       args = getline();
-      pid = fork();
 
       if (args[0] != NULL) {
          if (strcmp(args[0], "exit") == 0) {
             _exit(0);
-         }
-         else if (strcmp(args[0], "cd") == 0) {
-            chdir(args);
-         }
-         else {
-            commands(args[0], args);
+         } else {
+            if(args[1] == NULL){
+               newProc(args[0], args);
+            } else {
+               newProc(args[0], args+1);
+            }
          }
       }
    }
