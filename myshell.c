3320a32b01bd3a9b87d4275f2bbd6f2726148d45
jbarros23 <jbarros@ucsc.edu>
2015-01-20 21:02:35 -0800

    dd

---
+++ myshell.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <errno.h>
+#include <string.h>
+
+extern char **getline();
+
+pid_t pid;
+
+void commands (char *c, char **a) {
+   int status;
+   if (strcmp(c, "cd") == 0) {
+      printf(chdir(a[1]));
+   } 
+   else {
+      if (pid == 0) {
+         execvp(c, a);
+      }
+      else {
+         waitpid(pid, &status,0); 
+      }
+   }
+}
+
+int main (void) {
+   int i;
+   char **args;
+   
+   while(1) {
+      printf("myshell> "); 
+      args = getline();
+      pid = fork();
+
+      if (args[0] != NULL) {
+         if (strcmp(args[0], "exit") == 0) {
+            _exit(0);
+         }
+         else {
+            commands(args[0], args);
+         }
+      }
+   }
+}
